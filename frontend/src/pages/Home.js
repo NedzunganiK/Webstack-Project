d0704f567940125e1c7d2968f84f285f50cce097
Nedzunganik <nedzunganik8@gmail.com>
2025-01-29 15:12:53 +0200

    Home page component

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+cd 
+function Home() {
+	  return (
+		      <div>
+		        <h1>Welcome to Towing & Recovery</h1>
+		        <p>Reliable towing services at your fingertip!</p>
+		      </div>
+		    );
+}
+
+export default Home;
